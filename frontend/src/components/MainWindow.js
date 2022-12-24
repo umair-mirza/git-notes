c5749fc428324716b63a47695bafd13a53413536
Umair Mirza <umair_hp@hotmail.com>
2022-12-24 20:10:32 +0500

    Added the Notes Table with pagination

--- frontend/src/components/MainWindow.js
+++ frontend/src/components/MainWindow.js
@@ -1,12 +1,12 @@
 import React from "react"
-import GistsTable from "./GistsTable"
+import NotesTable from "./NotesTable"
 
 import "../App.scss"
 
 const MainWindow = () => {
   return (
     <div className="container top-bottom-space">
-      <GistsTable />
+      <NotesTable />
     </div>
   )
 }
